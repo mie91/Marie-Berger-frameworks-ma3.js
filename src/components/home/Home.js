add784ac16d9e3247b452378c640b3b41bc8519e
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-03-12 12:53:32 +0100

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,7 +6,7 @@ export function Home() {
     return (
         <>
             <HomeContent>
-                <Heading content="Header Example" />
+                <Heading content="Welcome" />
                 <p>Ex consectetur adipisicing anim velit non tempor non et dolor. Culpa quis aliquip dolor nostrud. Elit eiusmod ea aliquip aliquip laboris. Do in in sunt labore nulla tempor.</p>
                 <p>Cupidatat officia amet id mollit cupidatat sunt incididunt. Culpa id irure quis commodo commodo mollit esse id consectetur excepteur adipisicing. Dolore minim aliquip ex anim ipsum pariatur. Non fugiat velit veniam ipsum exercitation occaecat veniam irure proident commodo. Laboris pariatur elit occaecat pariatur enim eiusmod amet duis mollit. Reprehenderit id eu consectetur enim. Sint cillum officia ipsum est nulla quis pariatur in irure exercitation elit magna adipisicing.</p>
             </HomeContent>
